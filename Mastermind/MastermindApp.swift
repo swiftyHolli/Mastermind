e0c3f43fedcb38f6c988de0ac55bf527a3316e35
Holger Becker <h.becker@chhb.de>
2023-08-22 20:05:41 +0200

    Initial Commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Holger Becker on 22.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
